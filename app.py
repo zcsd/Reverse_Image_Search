ef32c36358c33638d1ed552a3118280bf84942fa
zcsd <itsme.2rszv@slmail.me>
2022-08-14 08:43:19 +0800

    update

--- app.py
+++ app.py
@@ -55,10 +55,3 @@ if __name__ == '__main__':
     http_server = WSGIServer(("0.0.0.0", 5000), app)
     print('Server started.')
     http_server.serve_forever() 
-    '''
-    app.run(
-      host ='0.0.0.0',
-      port = 5000,
-      debug = False
-    )
-    '''
